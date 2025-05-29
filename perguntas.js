ea599d92666a0d8392f0dac65a98cb278ed4913b
r4isales <raissa.sales@escola.pr.gov.br>
2025-05-29 20:42:46 +0000

    9

--- perguntas.js
+++ perguntas.js
@@ -20,3 +20,39 @@ criaCartao(
     'Como a Coca-Cola se chama na China e o que significa?',
     'Na China, a Coca-Cola se chama Kekoukele, que significa diversão saborosa'
 )
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'É verdade que a polícia usa Coca-Cola para limpar sangue?',
+    'Isso é mito, mas a Coca-Cola se mostrou eficaz para remover manchas de sangue em testes no programa Mythbusters'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Quantos sabores diferentes de Coca-Cola existem?',
+    'Existem pelo menos 16 sabores diferentes de Coca-Cola, incluindo versões Diet e Zero'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Quem inventou a Coca-Cola?',
+    'John Pemberton, um farmacêutico americano que originalmente criou uma bebida alcoólica com coca e noz de cola'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'O que foi a campanha MagiCans da Coca-Cola?',
+    'Uma campanha de 1990 onde latas premiadas cuspiram dinheiro e brindes, mas causaram problemas por conter líquidos desagradáveis'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'O que foi a Coca-Cola branca?',
+    'Uma versão sem coloração feita na Segunda Guerra para o Marechal soviético Georgy Zhukov, fã da bebida mas sem querer associar-se ao símbolo americano'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Por que a Coca-Cola do McDonald’s é considerada a melhor?',
+    'Porque usa xarope em recipientes de aço inox, água filtrada especial e sistemas refrigerados para manter o sabor ideal'
+)
